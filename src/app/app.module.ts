6ca8e939e8321340a172d2a45140c2180b3e807b
Vineet Joshi <jvineet64@gmail.com>
2024-01-24 00:07:57 +0530

    Login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TextFieldComponent } from '../components/text-field/text-field.component';
+import { FormsModule } from '@angular/forms';
+import { CustomButtonComponent } from '../components/custom-button/custom-button.component';
+import { LoginComponent } from '../pages/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextFieldComponent,
+    CustomButtonComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/components/text-field/text-field.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-text-field',
+  templateUrl: './text-field.component.html',
+  styleUrl: './text-field.component.css'
+})
+export class TextFieldComponent {
+  @Input() placeholder: string = 'Placeholder';
+  inputValue: string = '';
+}
